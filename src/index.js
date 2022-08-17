572002291f4061e580820a0834a7b7249ad0ffca
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-08-17 20:23:37 -0300

    Update exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
+import { useState } from 'react'
+
 import {
-  View, Text, StyleSheet, Dimension
+  View, 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  StatusBar, 
+  ScrollView,
+  Image,
+  Pressable
 } from 'react-native'
 
 const App = () => {
+
+  const [press, setPress] = useState(0)
+
   return(
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Nosso Primeiro APP</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollcontent}>
+        <View>
+          <Image
+            source={require('./img/mussum.jpeg')}
+            style={styles.picture}
+          />
+        </View>
+        <Text style={styles.textStyle}>
+          Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada.Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.Quem num gosta di mé, boa gentis num é.
+        </Text>
+        <Text style={styles.textStyle}>
+          Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada.Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.Quem num gosta di mé, boa gentis num é.
+        </Text>
+        <Text style={styles.textStyle}>
+          Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada.Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.Quem num gosta di mé, boa gentis num é.
+        </Text>
+        <Text style={styles.textStyle}>
+          Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada.Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.Quem num gosta di mé, boa gentis num é.
+        </Text>
+        <Text style={styles.textStyle}>
+          Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada.Manduma pindureta quium dia nois paga.Cevadis im ampola pa arma uma pindureta.Quem num gosta di mé, boa gentis num é.
+        </Text>
+        <View style={styles.textBox}>
+          <Text style={styles.textBoxContent}>
+            Pressionado {press} x
+          </Text>
+
+          <Pressable
+            onPress={() => setPress(press +1)}
+            style={ ({pressed}) => [
+              {
+                backgroundColor: pressed ? '#000' : '#fff',
+                fontSize: 22,
+                borderRadius: 10
+              }
+            ] }
+            >
+            {({pressed}) =>  (
+              <Text
+                style={{ color: pressed ? 'green' : 'red',
+                    fontSize: 35 ,
+                    textAlign: 'center'
+                  }}
+              >
+                Manda vê
+              </Text>
+            )}
+          </Pressable>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -15,10 +76,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000'
+    backgroundColor: '#e2e',
+    marginHeight: StatusBar.currentHeight
+  },
+  scrollcontent: {
+    paddingHorizontal: 15
+  },
+  picture: {
+    width: 160,
+    height: 160,
+    marginTop: 50
+  },
+  pressStyle: {
+    fontSize: 26,
+  },
+  textBox: {
+    marginTop: 20,
+    marginBottom: 20,
+    paddingVertical: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10
+  },
+  textBoxContent: {
+    fontSize: 20,
+    textAlign: 'center',
   },
   textStyle: {
     color: 'white',
+    marginTop: 32,
+    fontSize: 22,
   }
 })
 
